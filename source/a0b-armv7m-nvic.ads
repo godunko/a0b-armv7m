9eb837b4966daf329922301efb5eaf5b243270d5
Vadim Godunko <vgodunko@gmail.com>
2024-04-09 11:05:32 +0400

    Registers of the NVIC

---
+++ source/a0b-armv7m-nvic.ads
@@ -0,0 +1,77 @@
+--
+--  Copyright (C) 2024, Vadim Godunko <vgodunko@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+--
+
+pragma Restrictions (No_Elaboration_Code);
+
+pragma Ada_2022;
+
+with System.Storage_Elements;
+
+package A0B.ARMv7M.NVIC
+  with Preelaborate
+is
+
+   type ISER_Register is array (External_Interrupt_Number) of Boolean
+     with Pack, Object_Size => 496;
+
+   type ICER_Register is array (External_Interrupt_Number) of Boolean
+     with Pack, Object_Size => 496;
+
+   type ISPR_Register is array (External_Interrupt_Number) of Boolean
+     with Pack, Object_Size => 496;
+
+   type ICPR_Register is array (External_Interrupt_Number) of Boolean
+     with Pack, Object_Size => 496;
+
+   type IABR_Register is array (External_Interrupt_Number) of Boolean
+     with Pack, Object_Size => 496;
+
+   type IPR_Register is array (External_Interrupt_Number) of Priority_Value
+     with Pack, Object_Size => 3_968;
+
+--     type NVIC_Registers is record
+--        ISER : ISER_Register with Volatile;
+--        ICER : ICER_Register with Volatile;
+--        ISPR : ISPR_Register with Volatile;
+--        ICPR : ICPR_Register with Volatile;
+--        IABR : IABR_Register with Volatile;
+--        IPR  : IPR_Register with Volatile;
+--     end record;
+
+--     for NVIC_Registers use record
+--        ISER at 16#000# range 0 .. 495;
+--        ICER at 16#080# range 0 .. 495;
+--        ISPR at 16#100# range 0 .. 495;
+--        ICPR at 16#180# range 0 .. 495;
+--        IABR at 16#200# range 0 .. 495;
+--        IPR  at 16#300# range 0 .. 3_967;
+--     end record;
+
+--     NVIC_Base : constant System.Address :=
+--       System.Storage_Elements.To_Address (16#E000_E100#);
+
+--     NVIC : NVIC_Registers with Import, Address => NVIC_Base;
+
+   ISER : ISER_Register
+     with Volatile,
+          Address => System.Storage_Elements.To_Address (16#E000_E100#);
+   ICER : ICER_Register
+     with Volatile,
+          Address => System.Storage_Elements.To_Address (16#E000_E180#);
+   ISPR : ISPR_Register
+     with Volatile,
+          Address => System.Storage_Elements.To_Address (16#E000_E200#);
+   ICPR : ICPR_Register
+     with Volatile,
+          Address => System.Storage_Elements.To_Address (16#E000_E280#);
+   IABR : IABR_Register
+     with Volatile,
+          Address => System.Storage_Elements.To_Address (16#E000_E300#);
+   IPR  : IPR_Register
+     with Volatile,
+          Address => System.Storage_Elements.To_Address (16#E000_E400#);
+
+end A0B.ARMv7M.NVIC;
