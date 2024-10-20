bb69fef12d666db18c72839b852bb076c843674c
Vadim Godunko <vgodunko@gmail.com>
2024-10-20 14:46:54 +0400

    Rename package.

--- source/a0b-armv7m-scs-systick.ads
+++ source/a0b-armv7m-scs-systick.ads
@@ -11,7 +11,7 @@ with System.Storage_Elements;
 
 with A0B.Types;
 
-package A0B.ARMv7M.SCS.SYST
+package A0B.ARMv7M.SCS.SysTick
   with Preelaborate
 is
 
@@ -89,4 +89,4 @@ is
           Address => System.Storage_Elements.To_Address (16#E000_E01C#);
    --  SysTick Calibration value Register
 
-end A0B.ARMv7M.SCS.SYST;
+end A0B.ARMv7M.SCS.SysTick;
